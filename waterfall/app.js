0419d256cda42d91e00937c520a6671fe57e25b8
董芮宁 <dongruining@dongruiningdeMacBook-Pro.local>
2016-10-04 14:43:02 +0800

    second

---
+++ waterfall/app.js
@@ -0,0 +1,54 @@
+/**
+ * Created by dongruining on 16/7/3.
+ */
+$(document).ready(function(){
+   $(window).on("load",function(){
+       imgLocation();
+       var dataImg = {"data":[{"src":"1.jpg"},{"src":"2.jpg"},{"src":"3.jpg"},{"src":"4.jpg"},{"src":"5.jpg"}]};
+       window.onscroll = function(){
+           if(scrollside()){
+               $.each(dataImg.data,function(index,value){
+                   var box = $("<div>").addClass("box").appendTo($("#container"));
+                   var content = $("<div>").addClass("content").appendTo(box);
+                   $("<img>").attr("src","./img1/"+$(value).attr("src")).appendTo(content);
+               });
+               imgLocation();
+           }
+       };
+   });
+});
+
+
+function scrollside(){
+    var box = $(".box");
+    var lastboxHeight = box.last().get(0).offsetTop +Math.floor(box.last().height()/2);
+    var documentHeight = $(document).width();
+    var scrollHeight = $(window).scrollTop();
+    return (lastboxHeight<scrollHeight+documentHeight)?true :false;
+}
+
+function imgLocation(){
+    var box = $(".box");
+    var boxWidth = box.eq(0).width();
+    var num =Math.floor($(window).width()/boxWidth) ;
+    var boxArr=[];
+    box.each(function(index,value){
+        //console.log(index+"--"+value);
+        var boxHeight = box.eq(index).height();
+        if(index<num){
+            boxArr[index] = boxHeight;
+            //console.log(boxHeight);
+        }else{
+            var minboxHeight = Math.min.apply(null,boxArr);
+            var minboxIndex = $.inArray(minboxHeight,boxArr);
+            //console.log(minboxIndex);
+            //console.log(value);
+            $(value).css({
+                "position":"absolute",
+                "top":minboxHeight,
+                "left":box.eq(minboxIndex).position().left
+            });
+            boxArr[minboxIndex]+=box.eq(index).height();
+        }
+    });
+}
